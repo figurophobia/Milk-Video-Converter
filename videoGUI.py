8a48dfb15849294517abe90c4ccb66ea4c8f023f
Davidavirn <dasamiarea@gmail.com>
2025-01-06 00:49:41 +0100

    Ejecutable linux, videuGUI finished

--- videoGUI.py
+++ videoGUI.py
@@ -1,15 +1,18 @@
-import sys
 import os
-import subprocess
+
+# Establece la variable de entorno antes de importar PyQt5
+os.environ["QT_QPA_PLATFORM"] = "xcb"
+
+import sys
 import cv2
 import random
 from PIL import Image
+import subprocess
 from multiprocessing import Process, cpu_count
-import argparse
 import shutil
 import time
-from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QFileDialog, QCheckBox, QSpinBox, QComboBox)
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QFileDialog, QCheckBox, QSpinBox, QComboBox, QProgressBar)
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
 from PyQt5.QtGui import QFont, QColor, QPalette
 
 def get_video_fps(video_path):
@@ -32,7 +35,6 @@ def extract_frames_with_ffmpeg(video_path, output_folder, fps):
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
 
-    print(f"Extracting frames at {fps} FPS...")
     command = f"ffmpeg -y -i {video_path} -vf fps={fps} {output_folder}/frame%06d.jpg"
     subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
@@ -48,11 +50,9 @@ def apply_filter(image, compression, effect, milk_type, calidad, frame_path):
         punt = 100
 
     imag = image.convert('RGB')
-    #Sacar el nombre del archivo dada su ruta absoluta, quita la extension
     nombre = os.path.splitext(os.path.basename(frame_path))[0]
 
     if compression:
-        # Guarda la imagen con el mismo nombre en la carpeta 'filtered_frames'
         output_folder = 'filtered_frames'
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
@@ -67,7 +67,8 @@ def apply_filter(image, compression, effect, milk_type, calidad, frame_path):
             pixelRGB = imag.getpixel((x, y))
             R, G, B = pixelRGB
             brightness = sum([R, G, B]) / 3
-            new_pixel = (0, 0, 0, 255)  # Default black
+            new_pixel = (0, 0, 0, 255)
+
             if milk_type == 1:
                 if brightness <= 25:
                     new_pixel = (0, 0, 0, 255)
@@ -109,7 +110,7 @@ def apply_filter_to_frame_range(start, end, input_folder, output_folder, compres
             filtered_frame_path = os.path.join(output_folder, f"frame{frame_num:06d}.jpg")
             filtered_image.save(filtered_frame_path)
 
-def apply_filter_to_frames(input_folder, output_folder, compression=False, effect=False, milk_type=1, quality=90):
+def apply_filter_to_frames(input_folder, output_folder, compression=False, effect=False, milk_type=1, quality=90, progress_callback=None):
     """Applies the custom filter to each extracted frame and saves them to the output folder."""
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -137,38 +138,35 @@ def apply_filter_to_frames(input_folder, output_folder, compression=False, effec
         processes.append(process)
         process.start()
 
-    # Track progress
     while any(process.is_alive() for process in processes):
         og_files = len(os.listdir(input_folder))
         filtered_files = len(os.listdir(output_folder))
         if og_files > 0:
             progress = (filtered_files / og_files) * 100
-            print(f"\rProgress: {progress:.2f}%", end='', flush=True)
+            if progress_callback:
+                progress_callback.emit(int(progress))
         time.sleep(1)
 
     for process in processes:
         process.join()
 
-    print("\rProgress: 100.00% - FINISH!")
+    if progress_callback:
+        progress_callback.emit(100)
 
 def frames_to_video(input_folder, output_path, fps, original_video_path):
     """Converts a sequence of frames into a video file using ffmpeg and retains the original audio."""
     frame_files = [f for f in os.listdir(input_folder) if f.endswith('.jpg')]
     
     if not frame_files:
-        print("No frames found in the input folder.")
         return
     
-    # Ensure the frames are sorted correctly
     frame_files.sort(key=lambda x: int(x[5:-4]))
     
-    # Create a temporary text file listing all frames in the correct order
     with open('frames.txt', 'w') as f:
         for frame_file in frame_files:
             frame_path = os.path.join(input_folder, frame_file)
             f.write(f"file '{frame_path}'\n")
 
-    # Construct the ffmpeg command
     command = [
         'ffmpeg',
         '-y',
@@ -181,15 +179,45 @@ def frames_to_video(input_folder, output_path, fps, original_video_path):
         '-c:a', 'aac',
         '-strict', 'experimental',
         '-pix_fmt', 'yuv420p',
-        '-shortest',  # Ensure the video and audio lengths match
+        '-shortest',
         output_path
     ]
 
-    # Execute the ffmpeg command
     subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    
-    # Remove the temporary text file
     os.remove('frames.txt')
+
+class FilterThread(QThread):
+    progress = pyqtSignal(int)
+    finished = pyqtSignal()
+
+    def __init__(self, video_path, fps, milk_type, pointillism, compression, quality):
+        super().__init__()
+        self.video_path = video_path
+        self.fps = fps
+        self.milk_type = milk_type
+        self.pointillism = pointillism
+        self.compression = compression
+        self.quality = quality
+
+    def run(self):
+        extract_frames_with_ffmpeg(self.video_path, 'og', self.fps)
+        apply_filter_to_frames('og', 'filtered_frames', compression=self.compression, effect=self.pointillism, milk_type=self.milk_type, quality=self.quality, progress_callback=self.progress)
+        
+        for file in os.listdir('og'):
+            os.remove(f'og/{file}')
+
+        video_name, video_ext = os.path.splitext(self.video_path)
+        filtered_video_path = f"{video_name}_filtered{video_ext}"
+
+        frames_to_video('filtered_frames', filtered_video_path, self.fps, self.video_path)
+
+        for file in os.listdir('filtered_frames'):
+            os.remove(f'filtered_frames/{file}')
+
+        os.rmdir('filtered_frames')
+        os.rmdir('og')
+
+        self.finished.emit()
 
 class VideoFilterApp(QWidget):
     def __init__(self):
@@ -200,10 +228,19 @@ class VideoFilterApp(QWidget):
     def initUI(self):
         self.setWindowTitle("Milk Inside a Bag of Milk Video Filter")
         self.setGeometry(100, 100, 400, 300)
-        
+
         palette = QPalette()
-        palette.setColor(QPalette.Window, QColor(0, 0, 0))
-        palette.setColor(QPalette.WindowText, QColor(255, 255, 255))
+        palette.setColor(QPalette.Window, QColor("#040404"))  # Fondo de la ventana
+        palette.setColor(QPalette.WindowText, QColor("#7c138b"))  # Texto de la ventana
+        palette.setColor(QPalette.Base, QColor("#7c138b"))  # Fondo de los campos de entrada
+        palette.setColor(QPalette.AlternateBase, QColor("#5d0423"))  # Fondo alternativo
+        palette.setColor(QPalette.ToolTipBase, QColor("#5d0423"))  # Fondo de la herramienta
+        palette.setColor(QPalette.ToolTipText, QColor("#040404"))  # Texto de la herramienta
+        palette.setColor(QPalette.Text, QColor("#040404"))  # Texto en los campos de entrada
+        palette.setColor(QPalette.Button, QColor("#7c138b"))  # Fondo de los botones
+        palette.setColor(QPalette.ButtonText, QColor("#040404"))  # Texto de los botones
+        palette.setColor(QPalette.Highlight, QColor("#5d0423"))  # Color de resaltado
+        palette.setColor(QPalette.HighlightedText, QColor("#040404"))  # Texto resaltado
         self.setPalette(palette)
 
         font = QFont("Arial", 14, QFont.Bold)
@@ -225,6 +262,7 @@ class VideoFilterApp(QWidget):
         layout.addWidget(self.fps_checkbox)
 
         self.fps_spinbox = QSpinBox(self)
+        self.fps_spinbox.setFixedSize(80, 30)
         self.fps_spinbox.setRange(1, 60)
         self.fps_spinbox.setValue(30)
         self.fps_spinbox.setEnabled(False)
@@ -235,6 +273,7 @@ class VideoFilterApp(QWidget):
         layout.addWidget(self.milk_type_label)
 
         self.milk_type_combo = QComboBox(self)
+        self.milk_type_combo.setFixedSize(150, 30)
         self.milk_type_combo.addItem("Milk Type 1")
         self.milk_type_combo.addItem("Milk Type 2")
         layout.addWidget(self.milk_type_combo)
@@ -247,10 +286,19 @@ class VideoFilterApp(QWidget):
         layout.addWidget(self.compression_checkbox)
 
         self.quality_spinbox = QSpinBox(self)
+        self.quality_spinbox.setFixedSize(80, 30)
         self.quality_spinbox.setRange(0, 100)
         self.quality_spinbox.setValue(90)
         self.quality_spinbox.setEnabled(False)
         layout.addWidget(self.quality_spinbox)
+
+        self.progress_bar = QProgressBar(self)
+        layout.addWidget(self.progress_bar)
+
+        self.progress_label = QLabel("Progress: 0.00%", self)
+        self.progress_label.setFont(font)
+        self.progress_label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.progress_label)
 
         self.start_btn = QPushButton("Start Processing", self)
         self.start_btn.setFixedSize(150, 30)
@@ -279,39 +327,33 @@ class VideoFilterApp(QWidget):
         if self.video_path:
             self.select_video_btn.setText(f"Selected: {os.path.basename(self.video_path)}")
 
+    def update_progress(self, progress):
+        self.progress_bar.setValue(int(progress))
+        self.progress_label.setText(f"Progress: {progress:.2f}%")
+
+    def processing_finished(self):
+        self.progress_label.setText("Progress: 100.00% - FINISH!")
+        self.start_btn.setEnabled(True)
+
     def start_processing(self):
         if not self.video_path:
-            print("No video selected!")
             return
 
         fps = get_video_fps(self.video_path)
         if self.fps_checkbox.isChecked():
             fps = self.fps_spinbox.value()
 
-        extract_frames_with_ffmpeg(self.video_path, 'og', fps)
-
         milk_type = 1 if self.milk_type_combo.currentIndex() == 0 else 2
         pointillism = self.pointillism_checkbox.isChecked()
         compression = self.compression_checkbox.isChecked()
         quality = self.quality_spinbox.value()
 
-        apply_filter_to_frames('og', 'filtered_frames', compression=compression, effect=pointillism, milk_type=milk_type, quality=quality)
+        self.start_btn.setEnabled(False)
 
-        for file in os.listdir('og'):
-            os.remove(f'og/{file}')
-
-        video_name, video_ext = os.path.splitext(self.video_path)
-        filtered_video_path = f"{video_name}_filtered{video_ext}"
-
-        frames_to_video('filtered_frames', filtered_video_path, fps, self.video_path)
-
-        for file in os.listdir('filtered_frames'):
-            os.remove(f'filtered_frames/{file}')
-
-        os.rmdir('filtered_frames')
-        os.rmdir('og')
-
-        print("Processing completed!")
+        self.filter_thread = FilterThread(self.video_path, fps, milk_type, pointillism, compression, quality)
+        self.filter_thread.progress.connect(self.update_progress)
+        self.filter_thread.finished.connect(self.processing_finished)
+        self.filter_thread.start()
 
 def main():
     app = QApplication(sys.argv)
